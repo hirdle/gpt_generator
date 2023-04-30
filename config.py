9721256d0c70d6ec2f3ee2a354125534a0f16191
hirdle <ilyapower368@gmail.com>
2023-04-30 10:58:54 +0300

    new

--- config.py
+++ config.py
@@ -6,7 +6,5 @@ load_dotenv()
 API_TOKEN_OPENAI = os.getenv('API_TOKEN_OPENAI')
 API_TOKEN_TELEGRAM = os.getenv('API_TOKEN_TELEGRAM')
 
-
-
-main_channel = "-1001714222366"
+main_channel = "-1001856652818"
 admin_channel = "-1001848609338"
